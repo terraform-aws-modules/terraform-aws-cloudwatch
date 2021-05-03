bd60614437afc69d079b5849d1e324b63acd3b46
Anton Babenko <anton@antonbabenko.com>
2021-05-03 22:47:57 +0200

    feat: Added module for Cloudwatch log group

--- examples/complete-log-metric-filter-and-alarm/outputs.tf
+++ examples/complete-log-metric-filter-and-alarm/outputs.tf
@@ -1,3 +1,13 @@
+output "cloudwatch_log_group_name" {
+  description = "Name of Cloudwatch log group"
+  value       = module.log_group.cloudwatch_log_group_name
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "ARN of Cloudwatch log group"
+  value       = module.log_group.cloudwatch_log_group_arn
+}
+
 output "cloudwatch_log_metric_filter_id" {
   description = "The name of the metric filter"
   value       = module.log_metric_filter.cloudwatch_log_metric_filter_id
