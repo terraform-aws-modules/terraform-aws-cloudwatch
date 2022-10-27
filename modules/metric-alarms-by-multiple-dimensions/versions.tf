adbcb2027a24320d515017c5096944257bd4b0f0
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-27 15:03:40 -0400

    feat!: Bump Terraform to v1.0+ and AWS provider to v4.0+

--- modules/metric-alarms-by-multiple-dimensions/versions.tf
+++ modules/metric-alarms-by-multiple-dimensions/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.55"
+      version = ">= 4.0"
     }
   }
 }
--- wrappers/metric-alarm/versions.tf
+++ wrappers/metric-alarm/versions.tf
@@ -1,3 +1,3 @@
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 1.0"
 }
