e62e629d56eff754da2eb3bf761e53932bdd5f2c
Bryant Biggs <bryantbiggs@gmail.com>
2024-03-06 21:18:36 -0500

    fix: Update CI workflow versions to remove deprecated runtime warnings (#67)

--- wrappers/log-stream/main.tf
+++ wrappers/log-stream/main.tf
@@ -4,6 +4,6 @@ module "wrapper" {
   for_each = var.items
 
   create         = try(each.value.create, var.defaults.create, true)
-  name           = try(each.value.name, var.defaults.name, null)
   log_group_name = try(each.value.log_group_name, var.defaults.log_group_name, null)
+  name           = try(each.value.name, var.defaults.name, null)
 }
