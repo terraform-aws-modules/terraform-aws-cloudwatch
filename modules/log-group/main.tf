b12fbc8188f69ff0005f0ae3975bf77e24817d42
Eiji Kudo <outlook.best116@gmail.com>
2026-02-12 21:41:51 +0900

    style: Align attribute indentation in log-group module

--- modules/log-group/main.tf
+++ modules/log-group/main.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_log_group" "this" {
   count = var.create ? 1 : 0
 
-  name              = var.name
-  name_prefix       = var.name_prefix
-  retention_in_days = var.retention_in_days
-  kms_key_id        = var.kms_key_id
-  log_group_class   = var.log_group_class
+  name                        = var.name
+  name_prefix                 = var.name_prefix
+  retention_in_days           = var.retention_in_days
+  kms_key_id                  = var.kms_key_id
+  log_group_class             = var.log_group_class
   skip_destroy                = var.skip_destroy
   deletion_protection_enabled = var.deletion_protection_enabled
 
--- wrappers/log-group/main.tf
+++ wrappers/log-group/main.tf
@@ -3,12 +3,13 @@ module "wrapper" {
 
   for_each = var.items
 
-  create            = try(each.value.create, var.defaults.create, true)
-  kms_key_id        = try(each.value.kms_key_id, var.defaults.kms_key_id, null)
-  log_group_class   = try(each.value.log_group_class, var.defaults.log_group_class, null)
-  name              = try(each.value.name, var.defaults.name, null)
-  name_prefix       = try(each.value.name_prefix, var.defaults.name_prefix, null)
-  retention_in_days = try(each.value.retention_in_days, var.defaults.retention_in_days, null)
-  skip_destroy      = try(each.value.skip_destroy, var.defaults.skip_destroy, null)
-  tags              = try(each.value.tags, var.defaults.tags, {})
+  create                      = try(each.value.create, var.defaults.create, true)
+  deletion_protection_enabled = try(each.value.deletion_protection_enabled, var.defaults.deletion_protection_enabled, null)
+  kms_key_id                  = try(each.value.kms_key_id, var.defaults.kms_key_id, null)
+  log_group_class             = try(each.value.log_group_class, var.defaults.log_group_class, null)
+  name                        = try(each.value.name, var.defaults.name, null)
+  name_prefix                 = try(each.value.name_prefix, var.defaults.name_prefix, null)
+  retention_in_days           = try(each.value.retention_in_days, var.defaults.retention_in_days, null)
+  skip_destroy                = try(each.value.skip_destroy, var.defaults.skip_destroy, null)
+  tags                        = try(each.value.tags, var.defaults.tags, {})
 }
